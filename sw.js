84e1ed1ea13ff3b922489c934ea720bfbeef5681
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-08-18 07:06:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='emas-modern-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='emas-modern-settings-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
