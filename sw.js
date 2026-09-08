0755f3b9b838cea23781b00db0f977d3ec7eb194
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-08 15:04:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const C='emas-firebase-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='emas-barber-cuadre-real-fix-v3';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+  }
+});
